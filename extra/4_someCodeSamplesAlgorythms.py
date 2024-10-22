b13c6012a0bb14d790782fd6699d8d0b4fdd6f75
VSabadash <vitaliiseptember@gmail.com>
2024-10-22 13:49:21 +0300

    added updates OOP

---
+++ extra/4_someCodeSamplesAlgorythms.py
@@ -0,0 +1,133 @@
+import typing
+import time
+
+
+"""
+coords in the console
+"""
+class Player():
+
+    def __init__(self, x, y):
+        self.coords = (x, y)
+
+rows = 5
+cols = 5
+player = Player(1, 2)
+
+
+def drawCoors(player: Player):
+    for i in range(rows):
+        for j in range(cols):
+            if player.coords == (i, j):
+                print('x', end=" ")
+            else:
+                print(f"{i}{j}", end=" ")
+        print()
+
+
+def changeCoords():
+    x = 0
+    y = 0
+    while(True):
+        player.coords = (x, y)
+        drawCoors(player=player)
+        time.sleep(2)
+        y += 1
+
+
+# changeCoords()
+
+"""
+Methor resolution order
+"""
+class A:
+
+    class_attrs = None
+
+    def __init__(self, *args, **kwargs):
+        self.args, self.kwargs = args, kwargs
+        self.__test_arg = None
+
+    def __len__(self) -> int:
+        return len(self.args) + len(self.kwargs)
+
+    def __bool__(self) -> bool:
+        return bool(self.__test_arg)
+
+    def main(self) -> None:
+        print('Вызов внутри: A')
+
+    @property
+    def test_arg(self) -> typing.Any:
+        return self.__test_arg
+
+    @test_arg.setter
+    def test_arg(self, value: typing.Any):
+        self.__test_arg = value
+
+
+class B(A):
+
+    def main(self) -> None:
+        print('Вызов внутри: B до super')
+        super().main()
+        print('Вызов внутри: B после super')
+
+    @classmethod
+    def create(cls, *args, **kwargs) -> 'B':
+        return cls(*args, **kwargs)
+
+    @staticmethod
+    def get_test() -> str:
+        return 'test'
+
+
+class C(B):
+
+    def main(self) -> None:
+        print('Вызов внутри: C до super')
+        super().main()
+        print('Вызов внутри: C после super')
+
+
+class D(A):
+
+    def main(self) -> None:
+        print('Вызов внутри: D до super')
+        super().main()
+        print('Вызов внутри: D после super')
+
+
+class QWE(C, D):
+
+    def main(self) -> None:
+        print('Вызов внутри: QWE до super')
+        super().main()
+        print('Вызов внутри: QWE после super')
+
+qwe = QWE()
+# qwe.main()
+
+# print(QWE.mro())
+
+
+
+"""
+palyndrome
+"""
+def is_palyndrom(some_str: str):
+    s = some_str.lower().replace(' ', '')
+    return s == s[::-1]
+
+print(is_palyndrom("А роза упала на лапу Азора"))
+# проверка на полиндром
+
+
+"""
+# круговой обход списка даже если индекс вне длинны массива
+"""
+players = ['alice', 'ales', 'test2']
+index = 6
+index = (index - 1) % len(players)
+
+print(index)
